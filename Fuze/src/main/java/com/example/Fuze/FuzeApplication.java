5ccf70f5e9e686d657be7d9a9f4ef1bc103cc85c
BeenaKumariB <75854921+BeenaKumariB@users.noreply.github.com>
2021-01-04 13:40:16 +0530

    Fuze

---
+++ Fuze/src/main/java/com/example/Fuze/FuzeApplication.java
@@ -0,0 +1,13 @@
+package com.example.Fuze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuzeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuzeApplication.class, args);
+	}
+
+}
---
+++ Fuze/src/test/java/com/example/Fuze/FuzeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Fuze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuzeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
